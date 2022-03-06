9fbc0406b25367dbef9b72f4d94705eeff4e7a07
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-06 12:06:44 +0000

    feat(loginwindow): markup the window

---
+++ AMONICAirlinesDesktopApp/Views/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace AMONICAirlinesDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
