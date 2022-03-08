09f15ea551781fd5d31ef66b657c61d96ae075c4
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-08 08:31:49 +0000

    improve(viewtemplatesdictionary): move the view styles from windowservice

--- AMONICAirlinesDesktopApp/Services/WindowService.cs
+++ AMONICAirlinesDesktopApp/Services/WindowService.cs
@@ -1,7 +1,5 @@
 ﻿using AMONICAirlinesDesktopApp.ViewModels;
-using System;
 using System.Windows;
-using System.Windows.Media.Imaging;
 
 namespace AMONICAirlinesDesktopApp.Services
 {
@@ -12,16 +10,13 @@ namespace AMONICAirlinesDesktopApp.Services
             Window window = new Window
             {
                 WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                Icon = new BitmapImage(
-                new Uri($"pack://application:,,,"
-                        + "/Resources/DS2017_TP09_color.png"))
             };
             TViewModel viewModel = new TViewModel();
             window.Content = viewModel;
             window.DataContext = viewModel;
             window.Title = viewModel.Title;
             viewModel.OnRequestClose += () => window.Close();
-            window.ShowDialog();
+            _ = window.ShowDialog();
         }
 
         public void ShowWindow<TViewModel>()
@@ -30,17 +25,11 @@ namespace AMONICAirlinesDesktopApp.Services
             Window window = new Window
             {
                 WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                Icon = new BitmapImage(
-                new Uri($"pack://application:,,,"
-                        + "/Resources/DS2017_TP09_color.png"))
             };
             TViewModel viewModel = new TViewModel();
             viewModel.OnRequestClose += () => window.Close();
             window.Content = viewModel;
             window.DataContext = viewModel;
-            window.Icon = new BitmapImage(
-                new Uri($"pack://application:,,,"
-                        + "/Resources/DS2017_TP09_color.png"));
             window.Title = viewModel.Title;
             window.Show();
         }
