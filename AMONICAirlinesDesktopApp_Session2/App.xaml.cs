831e05c2d67319771e7168b346088fb32a634b40
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-11 11:40:19 +0000

    build(session2): add a project

---
+++ AMONICAirlinesDesktopApp_Session2/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AMONICAirlinesDesktopApp.Services;
+using AMONICAirlinesDesktopApp_Session2.ViewModels;
+using System.Windows;
+
+namespace AMONICAirlinesDesktopApp_Session2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            DependencyService.Register<MessageBoxFeedbackService>();
+            DependencyService.Register<WindowService>();
+            DependencyService.Get<IWindowService>()
+                .ShowWindow<FlightScheduleViewModel>();
+        }
+    }
+}
---
+++ AMONICAirlinesDesktopApp_Session2/ViewModels/FlightScheduleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using AMONICAirlinesDesktopApp.ViewModels;
+
+namespace AMONICAirlinesDesktopApp_Session2.ViewModels
+{
+    public class FlightScheduleViewModel : BaseViewModel
+    {
+        public FlightScheduleViewModel()
+        {
+            Title = "Manage Flight Schedules";
+        }
+    }
+}
