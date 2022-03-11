2464da2d80d1ba0c94649ead547694269837cb89
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-11 12:59:32 +0000

    feat(flightscheduleviewmodel): add filtering

--- AMONICAirlinesDesktopApp_Session2/ViewModels/FlightScheduleViewModel.cs
+++ AMONICAirlinesDesktopApp_Session2/ViewModels/FlightScheduleViewModel.cs
@@ -1,8 +1,10 @@
-﻿using AMONICAirlinesDesktopApp_Session2.ViewModels;
+﻿using AMONICAirlinesDesktopApp.Commands;
 using AMONICAirlinesDesktopApp_Session2.Models.Entities;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data.Entity;
 using System.Linq;
+using System.Windows.Input;
 
 namespace AMONICAirlinesDesktopApp_Session2.ViewModels
 {
@@ -11,7 +13,43 @@ namespace AMONICAirlinesDesktopApp_Session2.ViewModels
         public FlightScheduleViewModel()
         {
             Title = "Manage Flight Schedules";
-            Schedules = GetSchedules();
+            Airports = new ObservableCollection<Airport>
+            {
+                new Airport
+                {
+                    IATACode = "Все аэропорты"
+                }
+            };
+            GetAirports()
+                .ToList()
+                .ForEach(a =>
+                {
+                    Airports.Add(a);
+                });
+            FromAirport = ToAirport = Airports.FirstOrDefault();
+
+            SortTypes = new List<string>
+            {
+                "по дате и времени",
+                "по цене на билеты эконом класса",
+                "по подтверждённым / не подтверждённым"
+            };
+            CurrentSortType = SortTypes.First();
+            FilterSchedules();
+        }
+
+        /// <summary>
+        /// Получает коллекцию аэропортов.
+        /// </summary>
+        /// <returns>Коллекция аэропортов.</returns>
+        private IEnumerable<Airport> GetAirports()
+        {
+            using (SessionTwoEntities context = new SessionTwoEntities())
+            {
+                return context
+                    .Airports
+                    .ToList();
+            }
         }
 
         /// <summary>
@@ -31,12 +69,10 @@ namespace AMONICAirlinesDesktopApp_Session2.ViewModels
             }
         }
 
-        private IEnumerable<Airport> airports;
-
-        public IEnumerable<Airport> Airports
+        public ObservableCollection<Airport> Airports
         {
-            get => airports;
-            set => SetProperty(ref airports, value);
+            get;
+            set;
         }
 
         private IEnumerable<string> sortTypes;
@@ -93,6 +129,76 @@ namespace AMONICAirlinesDesktopApp_Session2.ViewModels
         {
             get => schedules;
             set => SetProperty(ref schedules, value);
+        }
+
+        private Command filterSchedulesCommand;
+
+        public ICommand FilterSchedulesCommand
+        {
+            get
+            {
+                if (filterSchedulesCommand == null)
+                {
+                    filterSchedulesCommand = new Command(FilterSchedules);
+                }
+
+                return filterSchedulesCommand;
+            }
+        }
+
+        /// <summary>
+        /// Фильтрует рейсы.
+        /// </summary>
+        private void FilterSchedules(object commandParameter = null)
+        {
+            var currentSchedules = GetSchedules();
+            if (FromAirport != ToAirport)
+            {
+                if (FromAirport?.ID != 0)
+                {
+                    currentSchedules = currentSchedules.Where(cs =>
+                    {
+                        return cs.Route.DepartureAirportID == FromAirport.ID;
+                    });
+                }
+                if (ToAirport?.ID != 0)
+                {
+                    currentSchedules = currentSchedules.Where(cs =>
+                    {
+                        return cs.Route.ArrivalAirportID == ToAirport.ID;
+                    });
+                }
+            }
+            if (OutboundDate != null)
+            {
+                currentSchedules = currentSchedules
+                       .Where(cs => cs.Date == OutboundDate.Value);
+            }
+            if (!string.IsNullOrWhiteSpace(FlightNumber))
+            {
+                currentSchedules = currentSchedules.Where(cs =>
+                {
+                    return cs.FlightNumber.Contains(FlightNumber);
+                });
+            }
+            switch (CurrentSortType)
+            {
+                case "по дате и времени":
+                    currentSchedules = currentSchedules
+                        .OrderBy(cs => cs.Date + cs.Time);
+                    break;
+                case "по цене на билеты эконом класса":
+                    currentSchedules = currentSchedules
+                        .OrderBy(cs => cs.EconomyPrice);
+                    break;
+                case "по подтверждённым / не подтверждённым":
+                    currentSchedules = currentSchedules
+                        .OrderBy(cs => cs.Confirmed);
+                    break;
+                default:
+                    break;
+            }
+            Schedules = currentSchedules;
         }
     }
 }
