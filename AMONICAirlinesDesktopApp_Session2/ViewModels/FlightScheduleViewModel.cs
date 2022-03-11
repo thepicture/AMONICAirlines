81156b4fbfa20a0907de150ec10cc757381e1842
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-11 11:58:40 +0000

    style(flightscheduleview): markup the page

--- AMONICAirlinesDesktopApp_Session2/ViewModels/FlightScheduleViewModel.cs
+++ AMONICAirlinesDesktopApp_Session2/ViewModels/FlightScheduleViewModel.cs
@@ -8,5 +8,37 @@ namespace AMONICAirlinesDesktopApp_Session2.ViewModels
         {
             Title = "Manage Flight Schedules";
         }
+
+        private System.Collections.IEnumerable airports;
+
+        public System.Collections.IEnumerable Airports { get => airports; set => SetProperty(ref airports, value); }
+
+        private System.Collections.IEnumerable sortTypes;
+
+        public System.Collections.IEnumerable SortTypes { get => sortTypes; set => SetProperty(ref sortTypes, value); }
+
+        private object fromAirport;
+
+        public object FromAirport { get => fromAirport; set => SetProperty(ref fromAirport, value); }
+
+        private object toAirport;
+
+        public object ToAirport { get => toAirport; set => SetProperty(ref toAirport, value); }
+
+        private object currentSortType;
+
+        public object CurrentSortType { get => currentSortType; set => SetProperty(ref currentSortType, value); }
+
+        private System.DateTime? outboundDate;
+
+        public System.DateTime? OutboundDate { get => outboundDate; set => SetProperty(ref outboundDate, value); }
+
+        private string flightNumber;
+
+        public string FlightNumber { get => flightNumber; set => SetProperty(ref flightNumber, value); }
+
+        private System.Collections.IEnumerable schedules;
+
+        public System.Collections.IEnumerable Schedules { get => schedules; set => SetProperty(ref schedules, value); }
     }
 }
